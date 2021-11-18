64d7ec160a91255d6fb29121b09ef8d4dc3b8749
Jans Ivans Jevdokimovs <jans.ivans@gmail.com>
2021-11-18 13:42:00 +0200

    Add local storage example.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,29 @@
-export const component: DraymanComponent = async () => {
+export const component: DraymanComponent = async ({ Browser, forceUpdate }) => {
+    let token = await Browser.getToken();
+
     return () => {
         return (
-            <h1>Hello, world!</h1>
+            <>
+                <h1>{token ? 'Token is set' : 'Token is not set'}</h1>
+                <div>
+                    <button
+                        onClick={async () => {
+                            token = '123';
+                            await Browser.saveToken({ token });
+                            await forceUpdate();
+                        }}
+                    >Set token
+                    </button>
+                    <button
+                        onClick={async () => {
+                            token = null;
+                            await Browser.removeToken();
+                            await forceUpdate();
+                        }}
+                    >Remove token
+                    </button>
+                </div>
+            </>
         )
     }
 }
